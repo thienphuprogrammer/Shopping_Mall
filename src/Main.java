9c9631841702943a13d5a3309f0aa022e6c28a82
thienphuprogrammer <thienphuprogrammer@gmail.com>
2023-05-17 09:49:33 +0700

    user object

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,17 @@ import CommonFunction.AdminObject;
 import CommonFunction.LoginObject;
 
 import java.util.Scanner;
+import Shopping_Mall.UserObject.User;
 
 public class Main {
     public static void main(String[] args) {
+        ListProduct listProduct = new ListProduct();
+        listProduct.loadListProduct("product.bin");
+
+
+        new User();
+
+        listProduct.saveListProduct("product.bin");
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Username: ");
